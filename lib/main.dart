2449257c3a75a1329c0f8ca48be58965f60574cb
Albinson Arias <arias.almonte@gmail.com>
2022-07-22 11:07:41 -0400

    Cambiar titulo

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'AAAAAA'),
+      home: const MyHomePage(title: 'Hola'),
     );
   }
 }
